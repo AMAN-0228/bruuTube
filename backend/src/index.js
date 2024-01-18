29a78898276cda4019dc625953054951c77de050
Aman <amansahu5051@gmail.com>
2024-01-18 13:48:54 +0530

    DB connection and Server initialization

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import {connectDB} from './db/index.js'
+import {app} from './app.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.on("error", (error) => console.log("Server failed to connect with database",error))
+    port = process.env.PORT||3000
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+})
+.catch(err => {
+    console.log("MongoDB connection failed!!",err)
+})

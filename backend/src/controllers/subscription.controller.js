776c9f67e9dd9acc0ecd72b87b5bcea5f6c4780d
Aman <amansahu5051@gmail.com>
2024-02-10 10:23:01 +0530

    adding subscription controller

---
+++ backend/src/controllers/subscription.controller.js
@@ -0,0 +1,18 @@
+import { Subscription } from "../models/subscription.model";
+import asyncHandler from "../utils/asyncHandler";
+
+const subscribeToggle = asyncHandler( async(req,res) =>{
+    const {subscriptionStatus, channel} = req?.body
+
+    if(!channel){
+        throw new ApiError(400, "channel is required")
+    }
+
+    if(subscriptionStatus){
+        // await Subscription
+    }
+})
+
+export {
+    subscribeToggle
+}
